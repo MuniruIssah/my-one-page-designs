0d82cc646b3c78210c1ece44fda5ca05b3041264
Muniru Issah <imuniru@st.ug.edu.gh>
2021-12-25 18:05:25 +0000

    added scrolling

--- src/projectClasses/LandingPages/SomeonesMarvel/sections/CharacterSection/index.js
+++ src/projectClasses/LandingPages/SomeonesMarvel/sections/CharacterSection/index.js
@@ -5,11 +5,17 @@ import {
   faCaretRight,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useRef } from "react";
 import CharacterCard from "../../components/CharacterCard";
 import { dummyspCharacters } from "../../utils";
 import "./styles.scss";
 const MovieCharacterSection = ({ characters = dummyspCharacters }) => {
+  const characterListRef = useRef();
+  const scroll = (scrollOffset) => {
+    characterListRef.current.scrollLeft += scrollOffset;
+    console.log(characterListRef?.current?.scrollLeft)
+
+  };
   return (
     <section className="movieCharacterSection">
       <div className="characterSectionHeader">
@@ -18,15 +24,15 @@ const MovieCharacterSection = ({ characters = dummyspCharacters }) => {
           <span>Character</span>
         </div>
         <div className="characterControls">
-          <button>
+          <button disabled={characterListRef?.current?.scrollLeft===0} onClick={() => scroll(-210)}>
             <FontAwesomeIcon icon={faAngleLeft} />
           </button>
-          <button>
+          <button onClick={() => scroll(210)}>
             <FontAwesomeIcon icon={faAngleRight} />
           </button>
         </div>
       </div>
-      <div className="characterSectionInner">
+      <div className="characterSectionInner" ref={characterListRef}>
         {characters.map((item, index) => (
           <CharacterCard key={index} {...item} />
         ))}
